5ded20bbf8ef187dc3a340694d6b969ad58c31e1
Rusham Bhatt <rusham@Rushams-MacBook-Air.local>
2025-04-18 17:43:15 -0400

    made slideshow feature in homepage and included a another section in home

--- hair-salon-portfolio/src/pages/Contact.jsx
+++ hair-salon-portfolio/src/pages/Contact.jsx
@@ -1,8 +1,28 @@
+import PhotoCard from "../components/PhotoCard"
 
 function Contact(){
-    return <div className="contact-empty">
-        <p> Coming soon!</p>
-    </div>
+
+    return (
+        <div className="contact">
+            <div className="contact-text">
+                <h1 className="intro">
+                    About Me: Sushma Bhatta
+                </h1> 
+    
+                <h2 className="aboutMe">
+                    I love to eat lots of food and drink lots of alcohol.
+                </h2>
+    
+                <h2 className="aboutMe">
+                    Oh! I am also really good at doing hair.
+                </h2>
+            </div>
+            
+            <div className="contact-photo">
+                <PhotoCard className="contact-item" photo={{url: "/photos/IMG_6241.jpg", title: "This is me!"}}/>
+            </div>
+        </div>
+    )
 }
 
 export default Contact
